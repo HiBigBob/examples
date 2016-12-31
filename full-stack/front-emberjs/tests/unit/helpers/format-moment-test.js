5b7fc806815ce22a29dd14834e8223764eae74f6
Adrien Bouttier <hibigbob@gmail.com>
2016-12-31 15:11:19 +0100

    Add test folders

---
+++ full-stack/front-emberjs/tests/unit/helpers/format-moment-test.js
@@ -0,0 +1,12 @@
+
+import { formatMoment } from 'front-emberjs/helpers/format-moment';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format moment');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = formatMoment([42]);
+  assert.ok(result);
+});
+
